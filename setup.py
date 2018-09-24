399e76d0c1b4bfaebf6f60d699006e0388f9ec3f
Alfredo Umfurer <a.umfurer@aristas.com.ar>
2018-09-24 10:24:34 -0300

    setup fixed

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    name="fermat",
-    version="0.0.1",
-)
+setup(name='fermat',
+      version='0.0.2',
+      description='library to compute fermat distance',
+      author='Facundo Sapienza',
+      author_email='f.sapienza@aristas.com.ar',
+      packages=find_packages(),
+    )
